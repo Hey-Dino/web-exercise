41d50dc5cfefa3cd898564bfad33254519f63b49
Hey-Dino <1037765457@qq.com>
2022-09-03 21:31:05 +0800

    【添加】Vue生命周期activated及deactivated

---
+++ Vue/CLI/vue_test/37_src_生命周期activated及deactivated/pages/Home.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h2>我是Home内容</h2>
+        <ul class="nav nav-tabs">
+            <li>
+                <router-link
+                    to="/home/news"
+                    class="list-group-item"
+                    active-class="active"
+                >News</router-link>
+            </li>
+            <li>
+                <router-link
+                    to="/home/messages"
+                    class="list-group-item"
+                    active-class="active"
+                >Message</router-link>
+            </li>
+        </ul>
+        <keep-alive include="News">
+            <router-view></router-view>
+        </keep-alive>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Home",
+};
+</script>
---
+++ Vue/CLI/vue_test/37_src_生命周期activated及deactivated/pages/News.vue
@@ -0,0 +1,39 @@
+<template>
+    <ul>
+        <li :style="{opacity}">欢迎学习Vue</li>
+        <li>News01<input type="text" /></li>
+        <li>News02<input type="text" /></li>
+        <li>News03<input type="text" /></li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name: "News",
+    data() {
+        return {
+            opacity: 1,
+        };
+    },
+    activated() {
+        this.timer = setInterval(() => {
+            console.log("$");
+            this.opacity -= 0.01;
+            if (this.opacity <= 0) {
+                this.opacity = 1;
+            }
+        }, 16);
+    },
+    deactivated() {
+        clearInterval(this.timer);
+    },
+};
+</script>
+
+<style scoped>
+    ul li:first-child {
+        font-size: 25px;
+    }
+</style>>
+    
+</style>
