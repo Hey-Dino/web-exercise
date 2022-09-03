0b121df1260e868783589f66ba7b5c9773a69f11
Hey-Dino <1037765457@qq.com>
2022-09-03 21:29:28 +0800

    【添加】Vue路由历史记录方式replace

---
+++ Vue/CLI/vue_test/34_src_reoute-link标签的replace属性/App.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <div class="row">
+            <Banner />
+        </div>
+        <div class="row">
+            <div class="col-xs-offset-2 col-xs-2">
+                <div class="list-group">
+                    <!-- <a
+                        href="javascript:;"
+                        class="list-group-item"
+                    >About</a>
+                    <a
+                        href="javascript:;"
+                        class="list-group-item active"
+                    >Home</a> -->
+                    <router-link
+                        replace
+                        to="/about"
+                        class="list-group-item"
+                        active-class="active"
+                    >About</router-link>
+                    <router-link
+                        replace="true"
+                        to="/home"
+                        class="list-group-item"
+                        active-class="active"
+                    >Home</router-link>
+                </div>
+            </div>
+            <div class="col-xs-6">
+                <div class="panel">
+                    <div class="panel-body">
+                        <router-view></router-view>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Banner from "./components/Banner.vue";
+
+export default {
+    name: "App",
+    components: {
+        Banner,
+    },
+};
+</script>
---
+++ Vue/CLI/vue_test/34_src_reoute-link标签的replace属性/pages/Messages.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <ul>
+            <li
+                v-for="m in messages"
+                :key="m.id"
+            >
+                <router-link :to="{
+                    name: 'detail',     
+                    params: {           
+                        id: m.id,
+                        title: m.title
+                    }
+                }">
+                    {{m.title}}
+                </router-link>
+            </li>
+        </ul>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Messages",
+    data() {
+        return {
+            messages: [
+                { id: "001", title: "message001" },
+                { id: "002", title: "message002" },
+                { id: "003", title: "message003" },
+            ],
+        };
+    },
+};
+</script>
