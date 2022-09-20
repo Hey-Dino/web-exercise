a9ed7a0ac7f3dd8c4fc3ef8d2bf94cd64432ec55
Hey-Dino <1037765457@qq.com>
2022-09-20 19:44:08 +0800

    【修改】修改vue.config.js配置文件的productionSourceMap属性，避免项目打包时生成暂时无用的map文件

--- Vue/project-SHP/app/vue.config.js
+++ Vue/project-SHP/app/vue.config.js
@@ -9,5 +9,7 @@ module.exports = defineConfig({
                 target: "http://gmall-h5-api.atguigu.cn"
             }
         }
-    }
+    },
+    // 控制项目打包时不生成map文件（map文件可以判断文件出错位置，但是对于上线的项目可能并不需要）
+    productionSourceMap: false,
 })
